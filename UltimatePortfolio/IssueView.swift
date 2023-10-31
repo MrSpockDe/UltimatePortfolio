60013614927c7409b7b391dcea644d136d86aadb
MrSpockDe <MrSpockDe@googlemail.com>
2023-10-31 15:49:25 +0100

    new issues and tags

---
+++ UltimatePortfolio/IssueView.swift
@@ -0,0 +1,18 @@
+//
+//  IssueView.swift
+//  UltimatePortfolio
+//
+//  Created by Albert on 14.10.23.
+//
+
+import SwiftUI
+
+struct IssueView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IssueView()
+}
---
+++ UltimatePortfolio/NoIssueView.swift
@@ -0,0 +1,18 @@
+//
+//  NoIssueView.swift
+//  UltimatePortfolio
+//
+//  Created by Albert on 14.10.23.
+//
+
+import SwiftUI
+
+struct NoIssueView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoIssueView()
+}
